23b1a13c007da0690d00b6facf10756876e725c6
antonylrds <42950733+antonylrds@users.noreply.github.com>
2021-01-19 21:19:43 -0300

    Upload de arquivo concluído

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -93,18 +93,6 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
               {transactions &&
                 transactions.map(transaction => (
                   <tr key={transaction.id}>
